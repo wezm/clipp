2a2570710505584572a154047a4ca978692540db
Wesley Moore <wes@wezm.net>
2024-05-25 11:02:35 +1000

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,30 @@
 #![forbid(unsafe_code)]
 mod providers;
 
-use std::{fmt::Display, sync::OnceLock};
+use std::{io, fmt::Display, sync::OnceLock};
 
-static CLIP: OnceLock<providers::Board> = OnceLock::new();
+static CLIP: OnceLock<io::Result<providers::Board>> = OnceLock::new();
 
 /// Copy text to the clipboard.
 pub fn copy(text: impl Display) {
-    CLIP.get_or_init(providers::provide).0(&format!("{text}"));
+    CLIP.get_or_init(providers::provide)
+        .unwrap()
+        .0(&format!("{text}")).unwrap()
+}
+
+/// Copy text to the clipboard.
+pub fn copy2(text: &str) -> io::Result<()> {
+    CLIP.get_or_init(providers::provide)?.0(&text)
 }
 
 /// Paste text from the clipboard.
 pub fn paste() -> String {
-    CLIP.get_or_init(providers::provide).1()
+    CLIP.get_or_init(providers::provide)
+        .unwrap()
+        .1().unwrap()
+}
+
+/// Paste text from the clipboard.
+pub fn paste2() -> io::Result<String> {
+    CLIP.get_or_init(providers::provide)?.1()
 }
--- src/providers.rs
+++ src/providers.rs
@@ -1,12 +1,12 @@
 //! implements different clipboard types
 use std::{
-    io::{Read, Write},
+    io::{self, Read, Write},
     process::{Command, Stdio},
 };
 
 pub trait Clipboard {
-    fn copy(text: &str);
-    fn paste() -> String;
+    fn copy(text: &str) -> io::Result<()>;
+    fn paste() -> io::Result<String>;
 }
 
 macro_rules! c {
@@ -23,37 +23,32 @@ macro_rules! c {
 }
 
 trait Eat {
-    fn eat(&mut self) -> String;
+    fn eat(&mut self) -> io::Result<String>;
 }
 
 impl Eat for Command {
-    fn eat(&mut self) -> String {
+    fn eat(&mut self) -> io::Result<String> {
         let mut s = String::new();
         self.stdout(Stdio::piped())
-            .spawn()
-            .expect("spawn ok")
+            .spawn()?
             .stdout
             .take()
-            .unwrap()
-            .read_to_string(&mut s)
-            .expect("read ok");
-        s
+            .expect("stdout")
+            .read_to_string(&mut s)?;
+        Ok(s)
     }
 }
 
 trait Put {
-    fn put(&mut self, s: impl AsRef<[u8]>);
+    fn put(&mut self, s: impl AsRef<[u8]>) -> io::Result<()>;
 }
 
 impl Put for Command {
-    fn put(&mut self, s: impl AsRef<[u8]>) {
-        let mut ch = self.stdin(Stdio::piped()).spawn().expect("spawn ok");
-        ch.stdin
-            .take()
-            .unwrap()
-            .write_all(s.as_ref())
-            .expect("write ok");
-        ch.wait().expect("proc ok");
+    fn put(&mut self, s: impl AsRef<[u8]>) -> io::Result<()> {
+        let mut ch = self.stdin(Stdio::piped()).spawn()?;
+        ch.stdin.take().expect("stdin").write_all(s.as_ref())?;
+        ch.wait()?;
+        Ok(())
     }
 }
 
@@ -61,68 +56,75 @@ impl Put for Command {
 pub struct PbCopy {}
 #[cfg(target_os = "macos")]
 impl Clipboard for PbCopy {
-    fn copy(text: &str) {
+    fn copy(text: &str) -> io::Result<()> {
         c!(pbcopy w).put(text)
     }
 
-    fn paste() -> String {
+    fn paste() -> io::Result<String> {
         c!(pbcopy r).eat()
     }
 }
 
 pub struct XClip {}
 impl Clipboard for XClip {
-    fn copy(text: &str) {
-        c!("xclip" "-selection" "c").put(text);
+    fn copy(text: &str) -> io::Result<()> {
+        c!("xclip" "-selection" "c").put(text)
     }
 
-    fn paste() -> String {
-        c!("xclip" "-selection" "c" "-o") // xcclip is complainy
+    fn paste() -> io::Result<String> {
+        c!("xclip" "-selection" "c" "-o") // xclip is complainy
             .stderr(Stdio::null())
             .stdout(Stdio::null())
-            .eat()
+            .eat() // If stdout is nulled does this work?
     }
 }
 
 pub struct XSel {}
 impl Clipboard for XSel {
-    fn copy(text: &str) {
-        c!("xsel" "-b" "-i").put(text);
+    fn copy(text: &str) -> io::Result<()> {
+        c!("xsel" "-b" "-i").put(text)
     }
 
-    fn paste() -> String {
+    fn paste() -> io::Result<String> {
         c!("xsel" "-b" "-o").eat()
     }
 }
 
 struct Wayland {}
 impl Clipboard for Wayland {
-    fn copy(text: &str) {
+    fn copy(text: &str) -> io::Result<()> {
         match text {
-            "" => assert!(
-                c!("wl-copy" "-p" "--clear").status().unwrap().success(),
-                "wl-copy fail"
-            ),
+            "" => c!("wl-copy" "-p" "--clear")
+                .status()?
+                .success()
+                .then_some(())
+                .ok_or_else(|| {
+                    io::Error::new(
+                        io::ErrorKind::Other,
+                        String::from("wl-copy was not successful"),
+                    )
+                }),
             s => c!("wl-copy" "-p").put(s),
         }
     }
 
-    fn paste() -> String {
+    fn paste() -> io::Result<String> {
         c!("wl-paste" "-n" "-p").eat()
     }
 }
 
 struct Klipper {}
 impl Clipboard for Klipper {
-    fn copy(text: &str) {
+    fn copy(text: &str) -> io::Result<()> {
         c!("qdbus" "org.kde.klipper" "/klipper" "setClipboardContents").arg(text);
+        Ok(())
     }
 
-    fn paste() -> String {
-        let mut s = c!("qdbus" "org.kde.klipper" "/klipper" "getClipboardContents").eat();
+    fn paste() -> io::Result<String> {
+        let mut s = c!("qdbus" "org.kde.klipper" "/klipper" "getClipboardContents").eat()?;
         assert!(s.ends_with('\n'));
         s.truncate(s.len() - 1);
-        s
+        Ok(s)
     }
 }
 
@@ -130,30 +132,33 @@ impl Clipboard for Klipper {
 struct Windows {}
 #[cfg(target_family = "windows")]
 impl Clipboard for Windows {
-    fn copy(text: &str) {
-        clipboard_win::set_clipboard_string(text).expect("set clip ok")
+    fn copy(text: &str) -> io::Result<()> {
+        clipboard_win::set_clipboard_string(text)?
     }
 
-    fn paste() -> String {
-        clipboard_win::get_clipboard_string().expect("get clip ok")
+    fn paste() -> io::Result<String> {
+        clipboard_win::get_clipboard_string()?
     }
 }
 
 struct Wsl {}
 
 impl Clipboard for Wsl {
-    fn copy(text: &str) {
-        c!("clip.exe").put(text);
+    fn copy(text: &str) -> io::Result<()> {
+        c!("clip.exe").put(text)
     }
 
-    fn paste() -> String {
-        let mut s = c!("powershell.exe" "-noprofile" "-command" "Get-Clipboard").eat();
+    fn paste() -> io::Result<String> {
+        let mut s = c!("powershell.exe" "-noprofile" "-command" "Get-Clipboard").eat()?;
         s.truncate(s.len() - 2); // \r\n
-        s
+        Ok(s)
     }
 }
 
-pub type Board = (for<'a> fn(&'a str), fn() -> String);
+pub type Board = (
+    for<'a> fn(&'a str) -> io::Result<()>,
+    fn() -> io::Result<String>,
+);
 
 fn get<T: Clipboard>() -> Board {
     (T::copy, T::paste)
@@ -165,39 +170,35 @@ fn has(c: &str) -> bool {
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .status()
-        .expect("ok")
-        .success()
+        .ok()
+        .map_or(false, |status| status.success())
 }
 
 fn wsl() -> bool {
-    if let Ok(s) = std::fs::read_to_string("/proc/version") {
-        if s.to_lowercase().contains("microsoft") {
-            return true;
-        }
-    }
-    false
+std::fs::read_to_string("/proc/version").map_or(false, |s| 
+        s.to_lowercase().contains("microsoft")
+)
 }
 
-pub fn provide() -> Board {
+pub fn provide() -> io::Result<Board> {
     #[cfg(target_family = "windows")]
     return get::<Windows>();
     #[cfg(target_os = "macos")]
     return get::<PbCopy>();
 
     if wsl() {
-        return get::<Wsl>();
+        return Ok(get::<Wsl>());
     }
-    assert!(std::env::var("DISPLAY").is_ok(), "no clipboard available");
-    if std::env::var("WAYLAND_DISPLAY").is_ok() && has("wl-copy") {
-        get::<Wayland>()
+    if std::env::var_os("WAYLAND_DISPLAY").is_some() && has("wl-copy") {
+        Ok(get::<Wayland>())
     } else if has("xsel") {
-        get::<XSel>()
+        Ok(get::<XSel>())
     } else if has("xclip") {
-        get::<XClip>()
+        Ok(get::<XClip>())
     } else if has("klipper") && has("qdbus") {
-        get::<Klipper>()
+        Ok(get::<Klipper>())
     } else {
-        panic!("no clipboard available");
+        Err(io::Error::new(io::ErrorKind::Other, String::from("no clipboard provided available")))
     }
 }
 
@@ -206,7 +207,7 @@ fn test() {
     macro_rules! test {
         ($clipboard:ty) => {
             <$clipboard>::copy("text");
-            assert_eq!(<$clipboard>::paste(), "text");
+            assert_eq!(<$clipboard>::paste().unwrap(), "text");
             <$clipboard>::copy("");
         };
     }
